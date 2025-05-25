c026c97067fda9c2ff3332fd19cce82e0e84226a
Luigi <bar24056@uvg.edu.gt>
2025-05-25 00:30:41 -0600

    Calcula los parametros medios de el historial de un usuario

--- main.py
+++ main.py
@@ -31,5 +31,61 @@ else:
     print("Check error details above")
 
 
+def parametros_medios():
+    try:
+        driver = GraphDatabase.driver(URI, auth=AUTH)
+        with driver.session() as session:
+
+            #obtener las 30 ultimas canciones del historial
+            records = session.run("match (u:User) -[:LISTENED_TO]-> (t:Track) where u.id = '1' LIMIT 30 return t")
+            #imprimir los parametros de la cancion
+
+
+            #crea la lista con valores
+            features = [
+
+                'acousticness',
+                'danceability',
+                'energy',
+                'instrumentalness',
+                'valence',
+                'avg_tempo'
+            ]
+
+            #crea un diccionario para cada atributo segun la lista features   
+            feature_values = {feature: [] for feature in features}
+
+            #imprime cada cancion del historial
+            for record in records:
+                track_node = record["t"]
+                print(track_node["name"])
+
+            # Guarda cada atributo en el diccionario
+                for feature in features:
+                    if feature in track_node:
+                        feature_values[feature].append(track_node[feature])
+                    else:#Si no lo encuentra avisa que ese dato no se hallo
+                        print(f"Error: El atributo {feature} no se encontro en la cancion {track_node['name']}")
+
+                # Calcula el promedio de cada atributo en el diccionario
+                averages = {}
+                for feature, values in feature_values.items():
+                    if values:  #Solo calcula si encuentra valores
+                        averages[feature] = np.mean(values)
+                    else:
+                        averages[feature] = None
+                
+                print("\nValores medios:")
+                for feature, avg in averages.items():
+                    print("Atributo:", feature , "-Valor: \n ", avg)
+                
+                return averages
+            
+            
+    except Exception as e:
+        logger.error(f"Error en la consulta: {e}")
+
+parametros_medios()
+
 
 
