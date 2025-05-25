ff3442be17aedd39d4e4d96d2f4bdd2cdd4ca65f
Luigi <bar24056@uvg.edu.gt>
2025-05-25 11:53:34 -0600

    registra las preferencias del usuario en un diccinario

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Datos de conexión 
-URI = "bolt://localhost:7687"  # o "neo4j://localhost" si usas Neo4j 4.3+
-AUTH = ("neo4j", "DBMSPassword")  # Usuario y contraseña
+URI = "bolt://localhost:7687"  
+AUTH = ("neo4j", "DBMSPassword") 
 
 def test_connection(): #Metodo extraido de la pagina de Neo4j for Python para verificar la conexion
     try:
@@ -75,9 +75,6 @@ def parametros_medios():
                     else:
                         averages[feature] = None
                 
-                print("\nValores medios:")
-                for feature, avg in averages.items():
-                    print("Atributo:", feature , "-Valor: \n ", avg)
                 
                 return averages
             
@@ -85,7 +82,53 @@ def parametros_medios():
     except Exception as e:
         logger.error(f"Error en la consulta: {e}")
 
-parametros_medios()
+
+
+def preferencias():
+
+    #crea un diccionario vacio para guardar las preferencias del usuario
+    preferencias = {
+        "favorite_genre": None,
+        "favorite_artist": None,
+        "disliked_genres": None,
+        "tempo_range": {"min": None, "max": None}
+    }
+
+    try:
+        driver = GraphDatabase.driver(URI, auth=AUTH)
+        with driver.session() as session:
+            #genero
+            result = session.run("MATCH (u:User)-[r:LIKES_GENRE]->(g) where u.id = '1' RETURN  g.name ")
+            if result.peek():
+                preferencias["favorite_genre"] = result.single()["g.name"]
+
+            #artista
+            result = session.run("MATCH (u:User)-[:FOLLOWS]->(a) where u.id = '1' return a.name")
+            if result.peek():
+                preferencias["favorite_artist"] = result.single()["a.name"]
+
+            #genero "disliked"
+            result = session.run("MATCH (u:User)-[r:DISLIKES_GENRE]->(g) where u.id = '1' RETURN  g.name ")
+            if result.peek():
+                preferencias["disliked_genres"] = result.single()["g.name"]
+
+            result = session.run("match (u:User) where u.id = '1' return u.tempo_max , u.tempo_min")
+            if result.peek():
+                record = result.single()
+                preferencias["tempo_range"] = {
+                    "min": record["u.tempo_min"],
+                    "max": record["u.tempo_max"]
+                }
+
+            return preferencias
+
+    except Exception as e:
+        logger.error(f"Error en la consulta: {e}")
+
+
+
+print(parametros_medios())
+print(preferencias())
 
 
 
